84ee2b6fb57c6feaf77d9c82a055473ac15fd23e
AnthonyMizal <202011040@gordoncollege.edu.ph>
2024-04-26 21:44:36 +0800

    update3

--- src/components/helpers/store.ts
+++ src/components/helpers/store.ts
@@ -1,5 +1,13 @@
 import { create } from "zustand";
-import { StatusState, Todostore } from "../../Types";
+import { StatusState } from "../../Types";
+import { TaskTypes } from "../../Types";
+import { v4 as uuidV4 } from "uuid";
+import { toast } from "sonner";
+type Todostore = {
+  todoList: TaskTypes[];
+  setTodoList: (tasks: TaskTypes[]) => void;
+  addTask: (title: string, category: string) => void;
+};
 
 export const useStatusFilter = create<StatusState>((set) => ({
   statusFilter: "all",
@@ -9,4 +17,22 @@ export const useStatusFilter = create<StatusState>((set) => ({
 export const useTodo = create<Todostore>((set) => ({
   todoList: [],
   setTodoList: (tasks) => set({ todoList: tasks }),
+  addTask: (title, category) =>
+    set((state) => {
+      const newTask = {
+        taskID: uuidV4(),
+        taskDate: new Date(),
+        taskCategory: category,
+        taskTitle: title,
+        taskStatus: false,
+      };
+
+      if (newTask.taskTitle.trim() === "") {
+        toast.error("Please enter a task title!");
+        return { ...state };
+      } else {
+        toast.success("Task has been created.");
+        return { todoList: [...state.todoList, newTask] };
+      }
+    }),
 }));
